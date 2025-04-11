9ee27bba2015254829bee00fede423403ef80059
Verao <gabrielmikaiverao@gmail.com>
2025-04-11 17:28:35 -0300

    fix: testes de acordo com arquivo padrão

--- test/intervals.e2e-spec.ts
+++ test/intervals.e2e-spec.ts
@@ -41,6 +41,20 @@ describe('IntervalsController (e2e)', () => {
                 expect(item.followingWin).toBeGreaterThan(item.previousWin);
             }
         }
+
+        expect(body.min).toContainEqual({
+            producer: 'Joel Silver',
+            interval: 1,
+            previousWin: 1990,
+            followingWin: 1991,
+        });
+
+        expect(body.max).toContainEqual({
+            producer: 'Matthew Vaughn',
+            interval: 13,
+            previousWin: 2002,
+            followingWin: 2015,
+        });
     });
 
     afterAll(async () => {
